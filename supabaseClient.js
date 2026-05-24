0d4d9121e885e037c921d0c866e60788d62c7182
Tandin Wangyel <tandinwangyel06@gmail.com>
2026-05-24 12:18:50 +0600

    Update all files and folders

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://YOUR_PROJECT.supabase.co"
-const supabaseKey = "YOUR_ANON_KEY"
+const supabase = createClient(
+  "https://deecrnfbvgbzyybqhywy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlZWNybmZidmdienl5YnFoeXd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNzk4MTAsImV4cCI6MjA5NDY1NTgxMH0.zO07GeHD-EW_6589vP07nTP95zFNIhp8YG57I5vWOf8"
+);
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    storage: window.localStorage,   // or window.sessionStorage
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+})
